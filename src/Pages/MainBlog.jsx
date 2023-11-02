12e3902f3ac9fa1c10af42d54108a2bb10052614
Pleasant Yomi <110311997+pleasantyomi@users.noreply.github.com>
2023-11-01 18:53:53 -0700

    Update MainBlog.jsx

--- src/Pages/MainBlog.jsx
+++ src/Pages/MainBlog.jsx
@@ -1,7 +1,7 @@
 import Blog from './Homepage/Blog'
 const MainBlog = () =>{
     return (
-        <div className="pt-[5.5rem]">
+        <div className="pt-[5.5rem] lg:pt-[7rem]">
             <Blog/>
         </div>
     )
